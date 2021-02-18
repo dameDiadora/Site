91104f80b71cdc6b4cc5c2b559e4b6dfcc5f411b
dameDiadora <61472518+dameDiadora@users.noreply.github.com>
2021-02-18 14:13:13 +0100

    Update Translationlist.js

--- docs/Translationlist.js
+++ docs/Translationlist.js
@@ -15,7 +15,7 @@ function Translationlist() {
         section.appendChild(itemHeader);
 
         var CSV, CSVLen, j, k, l;
-        var CSV = $.get("../" + CSVList[i] + ".csv", function(csv) {
+        var CSV = $.get("../アトヲニゴサズ.csv", function(csv) {
             // var CSV = csv.split(",");
             return csv;
         });
