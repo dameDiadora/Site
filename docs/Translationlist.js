44a8a0beda31e3ef32ec41ab5746fd8b2e12d864
dameDiadora <dameDiadora@protonmail.ch>
2021-02-21 11:44:43 +0100

    Moved tables into js file

--- docs/Translationlist.js
+++ docs/Translationlist.js
@@ -1,7 +1,7 @@
 function Translationlist() {
-    var patt = /.csv/g;
+    var CSVS=[["Japanese ","Romaji ","English ","はじめまして、僕はハレモノ","hajimemashite, boku wa haremono","Nice to meet you, I’m a tumor","触らないで　汚れたこの手に","sawaranaide yogoreta kono te ni","Don’t touch this dirty hand","心配しないで　すぐに消えるさ","shinpai shinaide sugu ni kieru sa","Don’t worry, I’ll disappear soon","何も残さないで","nani mo nokosanaide","Leaving nothing behind","","","","壮絶な哀しみや　絵になる悲劇など","souzetsu na kanashimi ya e ni naru higeki nado","Grand sorrow and perfect tragedies","同情の余地など何もない","doujou no yochi nado nani mo nai","And room for sympathy there’s no such things","どこかへ消えたいや","doko ka e kietai ya","I want to disappear somewhere","","","","ねぇ　どうか　僕を忘れてよ","nee douka boku o wasurete yo","Hey please forget me","消えたいんだ　恥ずかしいから","kietainda hazukashii kara","I want to disappear because it’s embarassing","誰にも迷惑はかけずに","dare ni mo meiwaku wa kakezu ni","Without causing anyone trouble","アトヲニゴサズ","ato o nigosazu","Leaving no trace","","","","他人が嫌いだ　でも　それ以上に","tanin ga kiraida demo sore ijou ni","I hate other but more than that","自分が嫌いだ　こっちを見ないで","jibun ga kiraida kocchi o minaide","I hate myself, don’t look at me","逃げ続けよう　隠れていよう","nige tsuzukeyou kakureteiyou","Let’s continue running away, let’s hide","ずっとずっと　いつまでも","zutto zutto itsu made mo","For a long, long time, forever","","","","このまま蝶になる","kono mama chou ni naru","Like this I’ll become a butterfly","もしくは　蝿になる","moshiku wa hae ni naru","Or maybe a fly","その方が　ふさわしいでしょ","sono hou ga fusawashii desho","That’s more fitting, right?","こんなこと　誰にも言えないな","konna koto dare ni mo ienai na","I can’t say these things to anyone","アトヲニゴサズ","ato o nigosazu","Leaving no trace","","","","どこかへ消えたいや","doko ka e kietai ya","I want to disappear somewhere","","","","あなたの記憶が　腐って落ちるまで","anata no kioku kusatte ochiru made","Until your memories rot and fall off","呼吸を忘れていよう","kokyuu o wasureteiyou","Let’s forget breathing","環境に配慮もした上で","kankyou ni hairyo mo shita ue de","Giving my surroundings full consideration","アトヲニゴサズ","ato o nigosazu","Leaving no trace","","","","あー　ねぇ　どうか　僕を忘れてよ","aa nee douka boku o wasurete yo","Aa hey please forget me","消えたいんだ　恥ずかしいから","kietainda hazukashii kara","I want to disappear because it’s embarassing","生きてた証はいらないな","ikiteta akashi wa iranai na","I don’t need proof that I lived","アトヲニゴサズ","ato o nigosazu","Leaving no trace"],["Japanese ","Romaji ","English ","絶対不可能、未来予知！","zettai fukanou, mirai yochi!","Definitely impossible, predicting the future!","","","","ああもう、天才ばっか","aa mou, tensai bakka","Aah, all these geniuses","虫みたいに蛍光灯に群がって","mushi mitai ni keikoutou ni muragatte","Swarming around the lamp like bugs","ブンブン、騒いでる","bunbun, sawaideru","Buzzing, making noise","声のでっかい、実際うっさい奇っ怪な奴ら","koe no dekkai, jissai ussai kikkai na yatsura","Loudmouthed, actually annoying and strange guys","","","","大体、虫達は","daitai, mushi tachi wa","Generally, the bugs","似たような主張を繰り返す","nita you na shuchou o kurikaesu","keep repeating the same similar claims","10年後の未来を見据えろって","juunengo no mirai o misuero tte","Saying to fix your eyes on the future 10 years from now","予知能力者？サイキッカー！？","yochi nouryokusha? saikikkaa!?","Fortune tellers? Psychics!?","","","","「勝ち組、目指しなよ。で、老後をお幸せに」","“kachigumi, mezashina yo. de, rougo o oshiawase ni”","“Don’t aim to win. Then, you’ll be happy in your old age”","夢見がちなのはさぁ","yume migachina no wa saa","The ones who keep dreaming","お前らの方だ！","omaera no hou da!","Are you people!","","","","1 or 8で進め！","wan oa eito de susume!","Move ahead with a 1 or 8!","先が見えなくたって","saki ga mienaku tatte","Even if you can’t see ahead","(ウォーウォーウォーウォー)","(woo woo woo woo)","(wo-oh wo-oh)","目隠しで走れ！","mekakushi de hashire!","Run blindfolded!","","","","5年・10年も先の","gonen, juunen mo saki no","There’s no way to know about","ことなんてわかるわけないよ","koto nante wakaru wake nai yo","things 5 or 10 years in the future","(ウォーウォーウォーウォー)","(woo woo woo woo)","(wo-oh wo-oh)","絶対不可能、未来予知！","zettai fukanou, mirai yochi!","Definitely impossible, predicting the future!","","","","ああもう、天才ばっか","aa mou, tensai bakka","Aah, all these geniuses","出来る前提、理想論を押し付ける","dekiru zentei, risouron o oshitsukeru","Assuming everything is possible, pushing that ideal","嘘じゃないなら、人類みんな","uso ja nai nara, jinrui minna","If it’s not a lie, then all of humanity would be","予知能力者！サイキッカー！","yochi nouryokusha! saikikkaa!","Fortune tellers! Psychics!","","","","「人生ゲーム 必勝法」","“jinsei geemu hisshouhou”","“A surefire way to win at life”","検索したって出てこないよ","kensaku shita tte detekonai yo","If you research it it won’t come out","現実見てないのは","genjitsu mitenai no ha","The one’s who don’t see reality","お前らの方だ！","omaera no hou da!","are you people!","","","","1 or 8で進め！","wan oa eito de susume!","Move ahead with a 1 or 8!","先は見えなくて当然","saki wa mienakute touzen","It’s natural that you can’t see the future","(ウォー、ウォー、ウォー、ウォー)","(woo, woo, woo, woo)","(wo-oh, wo-oh)","暗がりを走れ！","kuragari o hashire!","Run through the darkness!","","","","蛍光灯の明かりは","keikoutou no akari wa","Because the gleam of the lamp","数年で切れちゃうから","suunen de kirechau kara","will burn out in a few years","(ウォー、ウォー、ウォー、ウォー)","(woo, woo, woo, woo)","(wo-oh, wo-oh)","生涯安泰、世迷言！","shougai antai, yomaigoto!","Security for life, nonsense!","","","","生まれたことに意味なんて","umareta koto ni imi nante","Because there’s no way there’s meaning","ある訳ないんだから","aru wake nainda kara","to the fact that we were born","(ウォー、ウォー、ウォー、ウォー)","(woo, woo, woo, woo)","(wo-oh, wo-oh)","目隠しのままで","mekakushi no mama de","Still blindfolded","終わりの時まで","owari no toki made","Till the time of the end","","","","1 or 8で進め","wan oa eito de susume","Move ahead with a 1 or 8!","先が見えなくたって","saki ga mienaku tatte","Even if you can’t see ahead","(ウォー、ウォー、ウォー、ウォー)","(woo, woo, woo, woo)","(wo-oh, wo-oh)","目隠しで走れ","mekakushi de hashire","Run blindfolded!","5年、10年も先の","gonen, juunen mo saki no","There’s no way to know about","ことなんてわかるわけないよ","koto nante wakaru wake nai yo","things 5 or 10 years in the future","(ウォー、ウォー、ウォー、ウォー)","(woo, woo, woo, woo)","(wo-oh, wo-oh)","絶対不可能、未来予知！","zettai fukanou, mirai yochi!","Definitely impossible, predicting the future!"]]
     var i ;
-    var CSVList = ["アトヲニゴサズ"];
+    var CSVList = ["アトヲニゴサズ","ワン・オア・エイト？"];
     var CSVListLen = CSVList.length;
     var listCont = document.getElementById("List");
 
@@ -14,12 +14,8 @@ function Translationlist() {
         itemHeader.appendChild(item);
         section.appendChild(itemHeader);
 
-        var CSV, csv, CSVLen, j, k, l;
-        var CSV = $.get("アトヲニゴサズ.csv", function(csv) {
-            var CSV = csv.split('\n');
-            return CSV;
-        });
-        // CSV = ["1","2","3","4","5","6"]
+        var CSV, CSVLen, j, k, l;
+        var CSV = CSVS[i]
         CSVLen = CSV.length;
     
         var table = document.createElement("table");
@@ -27,7 +23,7 @@ function Translationlist() {
     
         for (j=0; j<3; j++) {
             var tableHeader = document.createElement("th");
-            var headerContent = document.createTextNode(CSV);
+            var headerContent = document.createTextNode(CSV[j]);
             tableHeader.appendChild(headerContent);
             tableRow.appendChild(tableHeader);
         }
@@ -37,7 +33,11 @@ function Translationlist() {
             var tableRow = document.createElement("tr");
             for (k=j, l=j+3; k < l ;k++) {
                 var tableCell = document.createElement("td");
-                var cellContent = document.createTextNode(CSV[k]);
+                if (CSV[k] != "") {
+                    var cellContent = document.createTextNode(CSV[k]);
+                } else {
+                    var cellContent = document.createTextNode('\u00A0');
+                }
                 tableCell.appendChild(cellContent);
                 tableRow.appendChild(tableCell);
             }
